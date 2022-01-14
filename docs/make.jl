0a4e4849e913ffb009fd72218ec210b9d3c2141a
Nivedita Rethnakar <n.paipriv@gmail.com>
2022-01-14 15:53:28 -0800

    removed Plots etc from docs project1

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,7 @@ makedocs(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://nivupai.github.io/InformationInequalities.jl",
         assets=String[],
+        collapselevel=1,
     ),
     pages=[
         "Home" => "index.md",
