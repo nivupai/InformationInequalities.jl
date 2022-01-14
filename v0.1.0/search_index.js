9f7280588e04caba21fd8b422887a032343ae886
Documenter.jl <documenter@juliadocs.github.io>
2022-01-14 07:12:48 +0000

    build based on e160fda

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = InformationInequalities","category":"page"},{"location":"#InformationInequalities","page":"Home","title":"InformationInequalities","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InformationInequalities.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [InformationInequalities]","category":"page"}]
+}
